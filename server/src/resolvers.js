e93490500ad190ad0ff82efb3e363bb53337872b
Rob Brennan <rob@therobbrennan.com>
2020-02-12 00:06:47 -0800

    Added error handling when attempting to create a new account in the GraphQL server

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,20 +1,49 @@
-import { neo4jgraphql } from "neo4j-graphql-js";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { pick, isNil } from "lodash";
+import { neo4jgraphql, cypherQuery } from 'neo4j-graphql-js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { pick, isNil } from 'lodash'
 
-import { createToken } from "./auth/auth";
+import { createToken } from './auth/auth'
 
 export const resolvers = {
   Mutation: {
     RegisterUser: async (object, params, context, resolveInfo) => {
-      const user = params;
-      user.password = await bcrypt.hash(user.password, 12);
+      const user = params
+      user.password = await bcrypt.hash(user.password, 12)
 
-      // TODO: Do not create an account if we already have one for the supplied email address!
+      /**
+       * IMPORTANT: How do we enforce a unique email for User nodes?
+       *
+       * Create constraint(s):
+       * $ CREATE CONSTRAINT ON (user:User) ASSERT user.email IS UNIQUE
+       * $ CREATE CONSTRAINT ON (user:User) ASSERT user.username IS UNIQUE
+       *
+       * To view constraints defined for your Neo4j database:
+       * $ CALL db.constraints
+       *
+       * To view labels for a node:
+       * $ MATCH (n) RETURN n, labels(n)
+       */
 
-      // We can safely create the user account
-      return neo4jgraphql(object, user, context, resolveInfo, true);
+      // Attempt to create the user account
+      return neo4jgraphql(object, user, context, resolveInfo, true)
+        .then(result => {
+          console.log(`result: ${JSON.stringify(result, null, 2)}`)
+          return result
+        })
+        .catch(e => {
+          // e => Neo4jError: Failed to invoke procedure `apoc.cypher.doIt`: Caused by: IndexEntryConflictException{propertyValues=( String("me@nomail.com") ), addedNodeId=-1, existingNodeId=31}
+          switch (true) {
+            case RegExp('existingNodeId').test(e):
+              throw new Error(
+                `An account already exists for this email address.`
+              )
+            default:
+              throw new Error(
+                `Whoops. We've encountered an unexpected error within our database server.`
+              )
+          }
+        })
     },
     Login: async (object, { email, password }, context, resolveInfo) => {
       const user = await neo4jgraphql(
@@ -24,14 +53,14 @@ export const resolvers = {
         resolveInfo,
         // Set debug to false unless you want to see plain text passwords in your log 😳
         false
-      );
+      )
       if (!user) {
-        throw new Error("No user with that email");
+        throw new Error('No user with that email')
       }
 
-      const valid = await bcrypt.compare(password, user.password);
+      const valid = await bcrypt.compare(password, user.password)
       if (!valid) {
-        throw new Error("Incorrect password");
+        throw new Error('Incorrect password')
       }
 
       const signedToken = await createToken(
@@ -39,16 +68,16 @@ export const resolvers = {
           user: { id: user.id, username: user.username }
         },
         context.SECRET
-      );
+      )
 
-      return `${signedToken}`;
+      return `${signedToken}`
     }
   },
   Query: {
     currentUser: async (object, params, context, resolveInfo) => {
-      const userID = context.user.id;
+      const userID = context.user.id
       if (isNil(userID)) {
-        return null;
+        return null
       }
 
       const { id, email, username } = await neo4jgraphql(
@@ -56,13 +85,13 @@ export const resolvers = {
         { user: userID },
         context,
         resolveInfo
-      );
+      )
 
       return {
         id,
         email,
         username
-      };
+      }
     }
   }
-};
+}
