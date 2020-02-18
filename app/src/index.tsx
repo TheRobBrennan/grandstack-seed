4fe6355b04e1fc21a9714bec2f432e0904826db3
Rob Brennan <rob@therobbrennan.com>
2020-02-17 17:34:45 -0800

    Default route now renders App component

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// App component
+import App from './App';
+
+// Apollo and GraphQL imports
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 
-const GRAPHQL_URI = 'http://localhost:8000/'
+// Apollo and GraphQL constants
+const GRAPHQL_URI = 'http://localhost:8000/';
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: GRAPHQL_URI,
@@ -12,3 +20,8 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link
 });
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
