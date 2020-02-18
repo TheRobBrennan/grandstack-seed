f173fe69c53f69279aff39b74ad5c4f784f91931
Rob Brennan <rob@therobbrennan.com>
2020-02-17 22:58:38 -0800

    Added simple HTML form using react-hook-form

--- app/src/components/login/Login.tsx
+++ app/src/components/login/Login.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';  // https://react-hook-form.com
 
-interface LoginProps {}
+export const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      username: 'rob@therobbrennan.com',
+      password: 'testtest',
+    }
+  });
 
-export const Login: React.FC<LoginProps> = () => {
+  const onSubmit = (data: any) => {
+    // TODO: Remove logging
+    console.log(JSON.stringify(data, null, 2));
+  };
+
   return (
     <>
       <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input name="username" ref={register} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input name="password" ref={register} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
