123f94f3e340da4adfb0f77601e9d34ddab99b94
Rob Brennan <rob@therobbrennan.com>
2020-02-17 17:41:26 -0800

    Moved Apollo setup and configuration to the App component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 
+// Apollo and GraphQL imports
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+// Apollo and GraphQL constants
+const GRAPHQL_URI = 'http://localhost:8000/graphql';
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: GRAPHQL_URI,
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link
+});
+
+
 function App() {
   return (
-    <div>[DEMO] GRANDstack | create-react-app</div>
+    <ApolloProvider client={client}>
+      <div>[DEMO] GRANDstack | create-react-app</div>
+    </ApolloProvider>
   );
 }
 
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,30 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// App component
 import App from './App';
 
-// Apollo and GraphQL imports
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
-
-// Apollo and GraphQL constants
-const GRAPHQL_URI = 'http://localhost:8000/graphql';
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: GRAPHQL_URI,
-});
-
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  cache,
-  link
-});
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <App />,
   document.getElementById('root')
 );
