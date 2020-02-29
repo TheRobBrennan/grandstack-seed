7abd273527fec41ebfdbea3893de2c873d6312cf
Rob Brennan <rob@therobbrennan.com>
2020-02-28 18:25:08 -0800

    Initial login/logout functionality complete

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 // Components
 import Login from './components/login/Login';
+import Placeholder from './components/placeholder/Placeholder';
 
 // Routing
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 // Apollo and GraphQL imports
 import { ApolloClient } from 'apollo-client';
@@ -36,12 +37,41 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache: new InMemoryCache()
 });
 
+const checkAuth = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    return true;
+  } else {
+    const queryParams = new URLSearchParams(window.location.search);
+    const altToken = queryParams.get("token");
+    if (altToken) {
+      localStorage.setItem("token", altToken);
+      return true;
+    }
+    return false;
+  }
+};
+
+const AuthRoute = ({ ...props }) => (
+  <Route
+    {...props}
+    render={() =>
+      checkAuth() ? <Placeholder /> : <Redirect to={{ pathname: "/login" }} />
+    }
+  />
+);
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" children={<Login />} />
+          <AuthRoute exact path="/" />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" render={() => {
+            localStorage.removeItem("token");
+            return <Redirect to={{ pathname: "/" }} />;
+          }} />
         </Switch>
       </BrowserRouter>
     </ApolloProvider>
