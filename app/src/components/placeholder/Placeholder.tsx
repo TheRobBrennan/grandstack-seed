2570152c6a89a8b7b569fe8f0b22e3ba2ad7b2d4
Rob Brennan <rob@therobbrennan.com>
2020-02-28 18:27:21 -0800

    Added ability to logout from the Placeholder component

--- app/src/components/placeholder/Placeholder.tsx
+++ app/src/components/placeholder/Placeholder.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 
 export const Placeholder = () => {
   return (
-    <div>Placeholder component works!</div>
+    <>
+      <div>Placeholder component works!</div>
+      <Link to="/logout">Logout</Link>
+    </>
   );
 };
 
