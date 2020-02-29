5465578a44b6f35c543f3951853e042cc4ec5e01
Rob Brennan <rob@therobbrennan.com>
2020-02-28 23:09:35 -0800

    Refactored logout function into a lib

--- app/src/components/logout/Logout.tsx
+++ app/src/components/logout/Logout.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Redirect } from "react-router-dom";
+import { logout } from '../../lib/logout';
 
 export const Logout = () => {
-  localStorage.removeItem("token");
+  logout();
   return <Redirect to={{ pathname: "/" }} />;
 };
 
