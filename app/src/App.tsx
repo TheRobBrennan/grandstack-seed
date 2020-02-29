84287cb6b9dc143f23b0dab27c5de79c9e0860ca
Rob Brennan <rob@therobbrennan.com>
2020-02-28 17:57:02 -0800

    Added authorization middleware

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,22 +8,33 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // Apollo and GraphQL imports
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink, concat } from "apollo-link";
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 // Apollo and GraphQL constants
 const GRAPHQL_URI = 'http://localhost:8000/graphql';
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: GRAPHQL_URI,
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_URI || GRAPHQL_URI
+});
+
+const authMiddleware = new ApolloLink((operation, forward: any) => {
+  // add the authorization to the headers
+  operation.setContext({
+    headers: {
+      authorization: localStorage.getItem("token") || null
+    }
+  });
+
+  return forward(operation);
 });
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  cache,
-  link
+  link: concat(authMiddleware, httpLink),
+  cache: new InMemoryCache()
 });
-
 
 function App() {
   return (
