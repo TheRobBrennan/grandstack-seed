b02919907cfcee5fc281ff922aa135161a2b7b60
Rob Brennan <rob@therobbrennan.com>
2020-02-28 22:57:24 -0800

    Refactored authMiddleware into a separate middleware file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -9,27 +9,17 @@ import Register from './components/register/Register';
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 // Apollo and GraphQL imports
-import { GRAPHQL_URI } from './config/constants'
 import { ApolloClient } from 'apollo-client';
-import { ApolloLink, concat } from "apollo-link";
+import { concat } from "apollo-link";
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { GRAPHQL_URI } from './config/constants';
+import { authMiddleware } from './middleware/auth-middleware';
 
 // Apollo and GraphQL constants
 const httpLink = new HttpLink({
   uri: process.env.REACT_APP_GRAPHQL_URI || GRAPHQL_URI
-});
-
-const authMiddleware = new ApolloLink((operation, forward: any) => {
-  // add the authorization to the headers
-  operation.setContext({
-    headers: {
-      authorization: localStorage.getItem("token") || null
-    }
-  });
-
-  return forward(operation);
 });
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
