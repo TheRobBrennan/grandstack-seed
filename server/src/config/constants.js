a0750e699e8d7eee8041cdf8e20fad03952359bd
Rob Brennan <rob@therobbrennan.com>
2020-02-28 21:57:17 -0800

    Refactored hard-coded GraphQL server URL

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -1,1 +1,2 @@
+export const GRAPHQL_URI = "http://localhost:8000/graphql"
 export const DEFAULT_JWT_SECRET = "thisisabadsecrettousebutitworksforanexample"
--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import { ApolloServer } from "apollo-server-express";
 import { makeAugmentedSchema } from "neo4j-graphql-js";
 
-import { DEFAULT_JWT_SECRET } from './config/constants'
+import { GRAPHQL_URI, DEFAULT_JWT_SECRET } from './config/constants'
 import { driver } from './config/neo4j'
 import { injectUser } from './middleware/inject-user'
 import { typeDefs } from "./graphql-schema";
@@ -46,6 +46,6 @@ server.applyMiddleware({ app, path: "/graphql" });
 // Open up a port and start the server on it
 app.listen({ port: process.env.GRAPHQL_LISTEN_PORT || 8000 }, () => {
   console.log(
-    `🚀 Server live at ${process.env.GRAPHQL_URI || "http://localhost:8000"} 🚀`
+    `🚀 Server live at ${process.env.GRAPHQL_URI || GRAPHQL_URI} 🚀`
   );
 });
