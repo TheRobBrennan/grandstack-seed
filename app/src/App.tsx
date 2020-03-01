5f484bc65da7d5bc93a8b65875cf4bc29a3ac6d1
Rob Brennan <rob@therobbrennan.com>
2020-02-29 16:18:04 -0800

    Refactored Apollo client configuration into a separate module

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Components
-import AuthRoute   from './components/auth-route/auth-route';
+import AuthRoute from './components/auth-route/auth-route';
 import Login from './components/login/Login';
 import Logout from './components/logout/Logout';
 import Register from './components/register/Register';
@@ -10,25 +10,8 @@ import Register from './components/register/Register';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // Apollo and GraphQL imports
-import { ApolloClient } from 'apollo-client';
-import { concat } from "apollo-link";
-import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { GRAPHQL_URI } from './graphql/apollo/apollo';
-
-// Middleware
-import { authMiddleware } from './middleware/auth-middleware';
-
-// Apollo and GraphQL constants
-const httpLink = new HttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_URI || GRAPHQL_URI
-});
-
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  link: concat(authMiddleware, httpLink),
-  cache: new InMemoryCache()
-});
+import { client } from './graphql/apollo/apollo';
 
 function App() {
   return (
